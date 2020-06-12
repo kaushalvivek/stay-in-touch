39afd6728b69ba84f4ae479d480f8094114d1139
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2020-06-12 13:02:12 +0530

    Production environment addition made https://github.com/fullstackreact/food-lookup-demo/issues/49

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ connection.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
